ce02606d2b643b3c07e1e55a5b61652ea2cab76f
Jamie Thompson <bishbashboshjt@gmail.com>
2022-12-06 11:45:10 +0100

    add optimal day 6

--- 2022/src/day06.scala
+++ 2022/src/day06.scala
@@ -2,7 +2,7 @@ package day06
 
 import locations.Directory.currentDir
 import inputs.Input.loadFileSync
-import scala.collection.mutable.Builder
+import scala.collection.mutable
 
 @main def part1: Unit =
   println(s"The solution is ${part1(loadInput())}")
@@ -19,10 +19,25 @@ def part2(input: String): Int =
   findIndex(input, n = 14)
 
 def findIndex(input: String, n: Int): Int =
-  val firstIndex = input.iterator
-    .zipWithIndex
-    .sliding(n)
-    .find(_.map(_(0)).toSet.size == n)
-    .get
-    .head(1)
+  val firstIndex = input.sliding(n).indexWhere(_.toSet.size == n)
+  firstIndex + n
+
+def findIndexOptimal(input: String, n: Int): Int =
+  val counts = mutable.HashMap.empty[Char, Int]
+  var previous = '#'
+  val firstIndex = input.iterator.sliding(n).indexWhere(cs =>
+    if previous == '#' then
+      cs.foreach(counts(_) = 1)
+    else if previous != cs.last then
+      counts.updateWith(previous) {
+        case Some(value) => if value == 1 then None else Some(value - 1)
+        case None => None // don't care if not in the
+      }
+      counts.updateWith(cs.last) {
+        case Some(value) => Some(value + 1)
+        case None => Some(1)
+      }
+    previous = cs.head
+    counts.size == n
+  )
   firstIndex + n
